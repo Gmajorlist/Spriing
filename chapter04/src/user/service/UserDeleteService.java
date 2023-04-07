b66b83beceaf914c597c77c86adc79495f2b4b44
Gmajorlist <darkmoster@naver.com>
2023-04-07 17:49:05 +0900

    0407

---
+++ chapter04/src/user/service/UserDeleteService.java
@@ -0,0 +1,16 @@
+package user.service;
+
+public class UserDeleteService implements UserService{
+
+	@Override
+	public void execute() {
+		삭제 할 아이디 입력 : ㅎㅎ
+		찾고자하는 아이디가 없음
+		
+		삭제 할 아이디 입력 : gonf
+		
+		DB의 내용을 삭제하엿습니다.
+		
+	}
+
+}
---
+++ chapter04/src/user/service/UserUpdateService.java
@@ -0,0 +1,20 @@
+package user.service;
+
+public class UserUpdateService implements UserService{
+
+	@Override
+	public void execute() {
+		수정 할 아이디 입력 : angel 
+			찾고자하는아이디가없어요
+			
+			수정할아이디입력 : hong
+			홍길동	hong	111
+			
+			수정 할 이름 입력 :
+			수정 할 비밀번호 입력 :
+			
+				
+			DB을 내용을 수정하였습니다.
+	}
+
+}
